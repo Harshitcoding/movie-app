3610c3f1651163baafafa881416e1d8d7a56c70a
Harshitcoding <khandelwalharshit96@gmail.com>
2025-12-31 14:37:53 +0530

    updated frontend to use live backend API

---
+++ frontend/src/config.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://movie-app-1-36pk.onrender.com";
--- frontend/src/services/movieService.js
+++ frontend/src/services/movieService.js
@@ -1,16 +1,16 @@
 import api from './api';
-
+import { API_URL } from '../config';
 const movieService = {
   // Get all movies with pagination
   getMovies: async (page = 1, limit = 10) => {
-    const response = await api.get(`api/movies?page=${page}&limit=${limit}`);
+    const response = await api.get(`${API_URL}/api/movies?page=${page}&limit=${limit}`);
     return response.data;
   },
 
   // Get sorted movies
   getSortedMovies: async (sortBy = 'title', order = 'asc', page = 1, limit = 10) => {
     const response = await api.get(
-      `api/movies/sorted?sortBy=${sortBy}&order=${order}&page=${page}&limit=${limit}`
+      `${API_URL}/api/movies/sorted?sortBy=${sortBy}&order=${order}&page=${page}&limit=${limit}`
     );
     return response.data;
   },
@@ -18,32 +18,32 @@ const movieService = {
   // Search movies
   searchMovies: async (query, page = 1, limit = 10) => {
     const response = await api.get(
-      `api/movies/search?query=${query}&page=${page}&limit=${limit}`
+      `${API_URL}/api/movies/search?query=${query}&page=${page}&limit=${limit}`
     );
     return response.data;
   },
 
   // Get single movie
   getMovieById: async (id) => {
-    const response = await api.get(`api/movies/${id}`);
+    const response = await api.get(`${API_URL}/api/movies/${id}`);
     return response.data;
   },
 
   // Add movie (admin only)
   addMovie: async (movieData) => {
-    const response = await api.post(`api/movies`, movieData);
+    const response = await api.post(`${API_URL}/api/movies`, movieData);
     return response.data;
   },
 
   // Update movie (admin only)
   updateMovie: async (id, movieData) => {
-    const response = await api.put(`api/movies/${id}`, movieData);
+    const response = await api.put(`${API_URL}/api/movies/${id}`, movieData);
     return response.data;
   },
 
   // Delete movie (admin only)
   deleteMovie: async (id) => {
-    const response = await api.delete(`api/movies/${id}`);
+    const response = await api.delete(`${API_URL}/api/movies/${id}`);
     return response.data;
   },
 };
